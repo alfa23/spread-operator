e6f69bcd5811a2cd51ed86008ab514df580aa192
alfa23 <paw235@gmail.com>
2022-04-11 00:16:30 +0000

    housekeeping

--- the-spread-operator.js
+++ the-spread-operator.js
@@ -4,60 +4,31 @@
  */
 
 // No spread operator
-// let arr1 = [1, 2, 3];
-// let arr2 = arr1;
-// arr2.push(4);
-// console.log("Second array", arr2);
-// console.log("First array", arr1);
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+arr2.push(4);
+console.log("Second array", arr2);
+console.log("First array", arr1);
 
-// // Copying an array
-// let arr3 = [4, 5, 6];
-// let arr4 = [...arr3];
+// Copying an array
+let arr3 = [4, 5, 6];
+let arr4 = [...arr3];
 
-// arr4.push(7);
-// console.log("Third array", arr3);
-// console.log("Fourth array", arr4);
+arr4.push(7);
+console.log("Third array", arr3);
+console.log("Fourth array", arr4);
 
-// // Copying an object
-// let obj1 = {a: 1, b: 2, c: 3};
-// let obj2 = {...obj1, d: 4};
-// let obj3 = {...obj1, b: 5}
-// console.log("First object", obj1);
-// console.log("Second object", obj2);
-// console.log("Third object", obj3);
+// Copying an object
+let obj1 = {a: 1, b: 2, c: 3};
+let obj2 = {...obj1, d: 4};
+let obj3 = {...obj1, b: 5}
+console.log("First object", obj1);
+console.log("Second object", obj2);
+console.log("Third object", obj3);
 
-// // Copying only part of an array/object
-// let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
-// let arr6 = [...arr1, {...obj1, b: 5}, ...arr3, "x", "y", "z"];
+// Copying only part of an array/object
+let arr5 = [...arr1, {...obj1}, ...arr3, "x", "y", "z"];
+let arr6 = [...arr1, {...obj1, b: 5}, ...arr3, "x", "y", "z"];
 
-// console.log("Fifth array", arr5);
-// console.log("Sixth array", arr6);
-
-
-let students = [
-    {
-        name: 'John',
-        subjects: ['maths', 'english', 'cad'],
-        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
-        results: {maths: 90, english: 75, cad: 87},
-    },
-    {
-        name: 'Emily',
-        subjects: ['science', 'english', 'art'],
-        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
-        results: {science: 93, english: 80, art: 95},
-    },
-    {
-        name: 'Adam',
-        subjects: ['science', 'maths', 'art'],
-        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
-        results: {science: 63, maths: 87, art: 95},
-    }
-];
-
-let subjects = [...students[0].subjects];
-const update = (item, val) => ( item = [ ...item, val ] );
-let updatedSubjects = update( subjects, "Electronics" );
-
-console.log(updatedSubjects);
-console.log(students[0])
+console.log("Fifth array", arr5);
+console.log("Sixth array", arr6);
